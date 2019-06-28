6f744045d8f1db9dbd52bf67cc58cfb5d33fa1c4
Karakog Davut <khd69@yahoo.com>
2019-06-28 09:11:34 +0200

    test

---
+++ Allocity/src/app/models/Data.model.ts
@@ -0,0 +1,10 @@
+export class Data {
+    constructor(public nom: string,
+        public prenom: string,
+        public age: number,
+        public adresse: string,
+        public photo: string,
+        public nomDuService: string,
+        public description: string,
+        public joursDisponnible: string, ) {}
+}
